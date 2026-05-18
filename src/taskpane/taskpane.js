72fef207552ca387601749bd6c62155a21e28e48
Narender Kumar <Narender.Kumar@multichoice.co.za>
2026-05-18 16:23:11 +0530

    add

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -3,9 +3,9 @@
 const supabaseClient =
     window.supabase.createClient(
 
-        process.env.SUPABASE_URL,
+        window.SUPABASE_URL,
 
-        process.env.SUPABASE_ANON_KEY
+        window.SUPABASE_ANON_KEY
     );
 
 Office.onReady(() => {
